30b8fdb101f0ef7291ee80ad949c9e8440220713
brian6382 <briankulei98@gmail.com>
2021-08-10 11:35:20 +0300

    Added first codes

---
+++ src/test/java/models/HeroesTest.java
@@ -0,0 +1,43 @@
+package models;
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HeroesTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Heroes.clearAllHeroes();
+    }
+
+    public Heroes setUpHero(){
+        return new Heroes("IronMan", "Smart", "Leader");
+    }
+    @Test
+    public void newHero_instantiates_correctly() throws Exception{
+        Heroes newHero = setUpHero();
+        assertEquals(true, newHero instanceof Heroes);
+        assertEquals("IronMan", newHero.getName());
+        assertEquals("Smart", newHero.getSuperPower());
+        assertEquals("Leader", newHero.getRole());
+    }
+
+    @Test
+    public void getAllHeroes_capturesAllHeroes_2() throws Exception{
+        Heroes first = setUpHero();
+        Heroes second = setUpHero();
+//        assertEquals(2, Heroes.getAllHeroes().size());
+    }
+
+
+}
